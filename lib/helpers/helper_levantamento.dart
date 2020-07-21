fc850690b6c9914628c9d9a5f37c68e15a09eab8
Vinícius Corrêa de Almeida <vinicius.correa@centralit.com.br>
2020-07-20 22:04:33 -0300

    Helper levantamento.

---
+++ lib/helpers/helper_levantamento.dart
@@ -0,0 +1,32 @@
+import 'package:app_inventario/models/dominio.dart';
+import '../models/levantamento.dart';
+
+List<Levantamento> helperLevantamento(List listaLevantamento) {
+  return List<Levantamento>.from(
+    (listaLevantamento).map(
+      (item) => Levantamento(
+        id: item['id'],
+        codigo: item['codigo'],
+        dataInicio: item['dataInicio'],
+        idOrganizacao: item['idOrganizacao'],
+        nome: item['nome'],
+        quantidadeEstruturas: item['quantidadeEstruturas'],
+        quantidadeTotalBens: item['quantidadeTotalBens'],
+        dominioStatusInventario: Dominio(
+          id: item['dominioStatusInventario']['id'],
+          nome: item['dominioStatusInventario']['nome'],
+          descricao: item['dominioStatusInventario']['descricao'],
+          chave: item['dominioStatusInventario']['chave'],
+          codigo: item['dominioStatusInventario']['codigo'],
+        ),
+        dominioTipoInventario: Dominio(
+          id: item['dominioTipoInventario']['id'],
+          nome: item['dominioTipoInventario']['nome'],
+          descricao: item['dominioTipoInventario']['descricao'],
+          chave: item['dominioTipoInventario']['chave'],
+          codigo: item['dominioTipoInventario']['codigo'],
+        ),
+      ),
+    ),
+  );
+}
